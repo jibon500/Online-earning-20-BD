1487373c1a0b46f49a8ad887d5586d0bf3970ee8
jibon500 <khan.sheikh.sagor@gmail.com>
2025-04-10 11:41:35 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,13 @@
-
+{
+  "type": "service_account",
+  "project_id": "investment-app-44dcb",
+  "private_key_id": "24165b7d050265a0ab4c410dd0ad99d6e2ed442f",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCrMT0mOJMNs0GL\nnVdSoMLuDTt2XArRDmKg9FVqJbFyTamTKQXVlLPjEivS+XwR+xYdAwSb3s+4X8ke\nng5ONgFtZipqSKX2/sL8RkxKPEbjLQHMt5sTAMz9Wx6aJ0c33KAo6/d+XED8bPzp\nAsHUsCprKOYRNU0tJzrqU+L7p3bTkoUvPrXKEmNQ9P+zclthcdSg/ElKck1mZA9q\nzsa4Xl1vLVQi7jBs4S2O+RRE3GlV0uCDbGbC0FaBrgJRezj1mVIUV/VwYXlsOhdu\nW5jLRZ/Q0GhlwoTG1Pq8R61vR6lBPuVZP9al3SwnMyU4FkV0sYxGRv1PauRKdGdX\nYDES72hZAgMBAAECggEAExEUnbNq43l92SIw1d3cYB2erA/Tz5Vx1DnqYnxov1GZ\ni6+ju/ZIM8f0FBcf47PKsngB1SdzjLY0ZQHXF4xpy8i3fCdzvqcAyg69U/5TKd27\nJ9UiQzNRYX160tIBm13qA6zGX69AOK0d3Qjm1Dy7uLwxJaUBX8BRDkTCEOPFtwoZ\nqGzVpSNlaCE4nokPTcMXjctcYm5d3eBLuYCdZuA/wUoBEcBX4BvOw/Th3IssiYWG\nJfE570IAKkT76xaURhbaSvnjLe3F1yMHGGfVpVOkbdz8MKCM7cO60ky8fsN4Uxb3\nRxr8urjaH88MmGOmgIs9PC7qWwYlfj2J2SHP527CIQKBgQDgCGZYulWQqKeb3UsA\nYeVZZUMB8fNa/P+aHVT/6klG8DRSIoe0VnUMaKES/Q5z452c4hH3/DuSOQx+eVTT\nvSzc7FK+nuCKThxFoP02JEWB+FOnlHY59yYyTcDwaWwRZRQW3ppm7PrEM/XWKKgN\nq5yR2NIX95d0uxzD7FpKspG3iQKBgQDDnqbW2awpfHvnhfSBriQstfqzDPWV209F\naPXWteUWTT+312d0GgLG/xfJHXikn9fubr1m11kM6Jbqqhd1oxLPCNo8kWBBRLfL\nocJN0Flf9xK8tc+DNHT0bbw0Rts7CiUzHa/nB7cAPr5eZdAyFFaUXtgEW5HeLmkl\nHhG6CWYmUQKBgDj5ieXIxZf7fFzEQ52+R9CbUxY8jRI3w11TVlmbILWcwwGcbHyt\nfCkCtuwYVUOdwGi0ibNSf2fpgEbt3mfW9YDdzRFTYHFr+f6vOZY6VT7/g27b79FA\nDIPAiG/br7MgzKIgTI4eeIvLVxr2zf5kBXaF+LBFtEvohiFBi5jxnuNZAoGALWlx\nNcNGckMRyIeSYWB5yV9dmZ9Si71jw6tAFt4XFnxMZHM+8vbu2h9x+CA9V4o76IAQ\nvAUWCScdkdB2rbpzrVhSwWjGPaCkQkOFBeduOBb1ZJWYXM8ar3uiC0i/sA8E377u\njOV/mDD4pJViJZ4PE1aJM0nGrRt9ET7Pwq5X5mECgYEAkg/TaXl7Z8xVpvsc3sIg\n3jbSHlkptNFxbFV4hGlCacCKUspxPhd62EopBYcvfiVZv6HxuAgkPXxS3f0ngSe1\nOqjsbXo9HZODYFmb19xzlgc0tAdEMSnnl/5lGC7Pu/3B1XbNNDmMpSyb8zLLHUsx\n9B/zn4n5Nad8Ny0YfbI0iuo=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@investment-app-44dcb.iam.gserviceaccount.com",
+  "client_id": "106727593447931346690",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40investment-app-44dcb.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
